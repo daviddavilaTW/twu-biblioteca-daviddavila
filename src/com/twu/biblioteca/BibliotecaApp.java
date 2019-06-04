5e146f6d8cad0c8f632f976226627f5e63b607c2
David <david.davila@thoughtworks.com>
2019-06-04 15:34:29 -0500

    Create function to improve the lecture.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -10,9 +10,9 @@ public class BibliotecaApp {
 
         Book bookArray[] = new Book[3];
 
-        bookArray[0] = new Book(1,"BookName1", "David", 2001, true);
-        bookArray[1] = new Book(2,"BookName2", "Alejandro", 2010, true);
-        bookArray[2] = new Book(3,"BookName3", "Melany", 2011, false);
+        bookArray[0] = new Book(0,"BookName1", "David", 2001, true);
+        bookArray[1] = new Book(1,"BookName2", "Alejandro", 2010, true);
+        bookArray[2] = new Book(2,"BookName3", "Melany", 2011, false);
 
 
         int optionChoosen = 5;
@@ -23,7 +23,7 @@ public class BibliotecaApp {
 
             System.out.println("Choose an option to continue:");
             System.out.println("1. List of books");
-            System.out.println("2. CheckOut a book");
+            System.out.println("2. Checkout a book");
 
             System.out.println("0. Quit");
 
@@ -32,25 +32,36 @@ public class BibliotecaApp {
 
             switch (optionChoosen) {
                 case 1:
-                    System.out.println("\n");
 
-                    // Problemas metiendolo en un metodo... (Refactor)
                     System.out.println("LIST OF BOOKS");
 
-                    for (int book = 0; book < bookArray.length; book++) {
-
-                        if (bookArray[book].isAvaliable()) {
-
-                            System.out.println(
-                                    bookArray[book].getBookName() + " --- " +
-                                            bookArray[book].getAuthorName() + " --- " +
-                                            bookArray[book].getPublicationYear());
-
-                        }
-                    }
+                    listBooks(bookArray);
 
                     break;
 
+
+                case 2:
+                    //Check out a book
+
+                    System.out.println("LIST OF AVALIABLE BOOKS");
+
+
+                    int optionCheckout = 6;
+
+                        while (optionCheckout != 5) {
+
+                            System.out.println("Choose a book to checkout to continue:");
+
+                            listAvaliableBooks(bookArray);
+
+                            System.out.println("5. Exit to main menu");
+
+                            optionCheckout = scanner.nextInt();
+
+
+
+                        }
+                    break;
                 case 0:
                     System.out.println("Bye!");
                     break;
@@ -63,5 +74,47 @@ public class BibliotecaApp {
         }
 
 
+
+
+
     }
+    public static void listBooks(Book[] bookArray){
+
+
+        for (int book = 0; book < bookArray.length; book++) {
+
+
+            System.out.println(
+                    bookArray[book].getBookName() + " --- " +
+                            bookArray[book].getAuthorName() + " --- " +
+                            bookArray[book].getPublicationYear());
+
+
+        }
+    }
+    public static void listAvaliableBooks(Book[] bookArray){
+
+        for (int book = 0; book < bookArray.length; book++) {
+
+            if (bookArray[book].isAvaliable()) {
+
+                System.out.println(
+                        bookArray[book].getIDBook() + ". " +
+                                bookArray[book].getBookName() + " --- " +
+                                bookArray[book].getAuthorName() + " --- " +
+                                bookArray[book].getPublicationYear());
+
+            }
+
+
+
+        }
+    }
+
+    public void changeBookStatus() {
+
+
+    }
+
+
 }
