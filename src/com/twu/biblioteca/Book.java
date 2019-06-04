7485ff43ff2591502cd261bf3e401f0def10dd9f
David <david.davila@thoughtworks.com>
2019-06-04 11:22:39 -0500

    Book class created. Getters and setters added.

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca;
+
+public class Book {
+    private String bookName;
+    private String authorName;
+    private int publicationYear;
+
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
