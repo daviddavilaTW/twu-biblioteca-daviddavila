552898d7032bc31d48276b2f74556e07052ec355
David <david.davila@thoughtworks.com>
2019-06-04 09:24:32 -0500

    View a list of all books. Done

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,5 +4,10 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        System.out.println("\n \n");
+
+        System.out.println("Book 1 \nBook 2 \nBook 3");
+
+
     }
 }
