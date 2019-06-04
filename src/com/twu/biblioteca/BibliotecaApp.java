7a20ee1a3dc9e57cbfa322d83c3197c033d51a9f
David <david.davila@thoughtworks.com>
2019-06-04 14:00:06 -0500

    View a main menu of options. Done

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,5 +1,7 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
@@ -12,13 +14,27 @@ public class BibliotecaApp {
         bookArray[1] = new Book("BookName2", "Alejandro", 2010);
         bookArray[2] = new Book("BookName3", "Melany", 2011);
 
+        System.out.println("Choose an option to continue:");
+        System.out.println("1. List of books");
 
+        String optionChoised;
+
+        Scanner scanner = new Scanner(System.in);
+        optionChoised = scanner.nextLine();
+        System.out.println("You choose the option " + optionChoised);
+
+
+
+        // Problemas metiendolo en un metodo... (Refactor)
+
+        System.out.println("\n");
+
+        System.out.println("LIST OF BOOKS");
         for (int book = 0; book < bookArray.length; book++) {
             System.out.println(
                     bookArray[book].getBookName() + " --- " +
                             bookArray[book].getAuthorName() + " --- " +
                             bookArray[book].getPublicationYear());
-            System.out.println("\n");
         }
 
 
